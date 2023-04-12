196e4bb53ced8b7ef3953c1938b46aeb9194ac39
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-04-12 21:07:58 +0530

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-secretstore',
-    version='1.4.3',
+    version='1.4.4',
     description='Netbox Secret Store',
     long_description='A Secret store for NetBox',
     url='https://github.com/dansheps/netbox-secretstore/',
